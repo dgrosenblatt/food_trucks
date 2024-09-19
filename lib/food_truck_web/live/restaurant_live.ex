f02d03f49d536602005b0bd38d3fa0d608874fde
Danny Rosenblatt <dgrosenblatt@gmail.com>
2024-09-19 15:02:55 -0400

    Add some emojis and gradients

--- lib/food_truck_web/live/restaurant_live.ex
+++ lib/food_truck_web/live/restaurant_live.ex
@@ -13,18 +13,9 @@ defmodule FoodTruckWeb.RestaurantLive do
     socket =
       socket
       |> assign(:restaurants, restaurants)
-      |> assign(:images, images())
       |> assign(:votes, votes)
 
     {:ok, socket}
-  end
-
-  # stock food/restaurant images
-  def images do
-    [
-      "https://media.istockphoto.com/id/1369489882/photo/variety-of-vegan-plant-based-protein-food.jpg?s=612x612&w=is&k=20&c=49K_SXkPRVTiOZMPGJLB_bplC7hAHZuDQv1Z9k4Yww0=",
-      "https://media.istockphoto.com/id/1387721011/photo/close-up-of-woman-eating-omega-3-rich-salad.jpg?s=612x612&w=is&k=20&c=W-RcE7BDYmR6rKxwfpC9lxIzYHin7ePrRK8c16khKQM="
-    ]
   end
 
   def handle_event("vote", params, socket) do
